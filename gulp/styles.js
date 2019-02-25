b92c3aab5727968d3c60d8f3f18b0aed421130ba
Peterson <contact@peterson.by>
2019-02-25 18:00:11 +0300

    feat: add postcss & autoprefixer

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,12 +2,19 @@ const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 
 gulp.task('styles', function() {
+	const plugins = [
+		autoprefixer()
+	];
+	
   return gulp.src('src/css/**/*.css')
 		.pipe(concat('style.css'))
+		.pipe(postcss(plugins))
 		.pipe(gulpIf(!isDev, csso()))
 	  .pipe(gulp.dest('build'));
 });
